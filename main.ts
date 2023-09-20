c973ec023e40eda130f2478a6700d746415b7c70
Jonah-StPierre <144264031+Jonah-StPierre@users.noreply.github.com>
2023-09-20 13:12:11 -0400

    Typescript version

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jonah S
+ * Created on: Sep 2023
+ * This program can preform basic math
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+basic.showString("A rectangle has dimensions 5 cm & 3 cm.")
+
+basic.showString(("The perimiter would be") + (2 * (5 + 3)).toString())
+
+basic.showString(("The area would be") + (3 * 5).toString())
