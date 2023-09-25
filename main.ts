7e0f2d52db8a82b25edddd849e3ac73c9cbae984
Jonah-StPierre <144264031+Jonah-StPierre@users.noreply.github.com>
2023-09-24 22:04:02 -0400

    Typescript version

--- main.ts
+++ main.ts
@@ -14,6 +14,6 @@ const width = 3
 const area = length * width
 const perimiter = 2 * (length + width)
 
-basic.showString('A rectangle has dimensions 5cm and 3cm.') 
+basic.showString('A rectangle has dimensions 5cm and 3cm.')
 basic.showString('The perimiter would be' + perimiter.toString() + 'cm')
 basic.showString('The area would be' + area.toString() + 'cm')
