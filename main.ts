9d217200038cf575d10480da57372047259b343a
Jonah-StPierre <144264031+Jonah-StPierre@users.noreply.github.com>
2023-09-24 21:55:27 -0400

    Typescript version

--- main.ts
+++ main.ts
@@ -8,12 +8,12 @@
 basic.clearScreen()
 basic.pause(1000)
 
-let length = 5 
-let width = 3
+const length = 5 
+const width = 3
 
-let area = length * width
-let perimiter = 2 * (length + width)
+const area = length * width
+const perimiter = 2 * (length + width)
 
-basic.showString("A rectangle has dimensions 5cm and 3cm.") 
-basic.showString("The perimiter would be" + perimiter + "cm") 
-basic.showString("The area would be" + area + "cm")
+basic.showString('A rectangle has dimensions 5cm and 3cm.') 
+basic.showString('The perimiter would be' + perimiter.toString() + 'cm' )
+basic.showString('The area would be' + area.toString() + 'cm')
